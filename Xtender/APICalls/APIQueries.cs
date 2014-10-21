f12800edd05ab7f3714a43a3e1a3f47d3bfc3d97
Cheesegeezer <daveybryce@hotmail.co.uk>
2014-10-21 13:22:20 +0100

    1.0.0.12 Add New API Auth

--- Xtender/APICalls/APIQueries.cs
+++ Xtender/APICalls/APIQueries.cs
@@ -23,30 +23,19 @@ namespace Xenon.APICalls
                 return Kernel.ApiClient.DashboardUrl.Split(new string[] { "dashboard" }, StringSplitOptions.None)[0];
         }
         
-        //Mandatory call to GET and deserialize JSON items from the query string(Pretty standard HTTP call)
-        private ItemsResult GetItemsResultAPIRespone(string queryUrl)
-        {
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(queryUrl);
-            request.Method = "GET";
-            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
-            {
-                return DeserializeFromStream<ItemsResult>(response.GetResponseStream());
-            }
-        }
-
         //Guid = UserId
         public ItemsResult NextUpAPIQuery(Guid guid)
         {
             string query = "&Limit=10&Fields=Name%2COverview%2CIsEpisode%2COfficialRating%2CStatus%2CPrimaryImageAspectRatio&format=Json"; //must include "&format=Json" in order to allow for the items to be read.
             string queryUrl = string.Format("{0}Shows/NextUp?UserId={1}{2}", APIUrl(), guid, query); //Query Format taken from Swagger
-            return GetItemsResultAPIRespone(queryUrl);//Interrogate the API based on the query string.
+            return Kernel.ApiClient.GenericApiQuery(queryUrl);//Interrogate the API based on the query string.
         }
 
         public ItemsResult UpComingQuery(Guid guid)
         {
             string query = "&Fields=Name%2CId%2CSeriesName%2CIndexNumber%2C%20ParentIndexNumber%2CAirTime%2CSeriesStudio%2C%20PremiereDate%2CParentBackdropItemId&format=Json"; //must include "&format=Json" in order to allow for the items to be read.
             string queryUrl = string.Format("{0}Shows/Upcoming?UserId={1}{2}", APIUrl(), guid, query); //Query Format taken from Swagger
-            return GetItemsResultAPIRespone(queryUrl);//Interrogate the API based on the query string.
+            return Kernel.ApiClient.GenericApiQuery(queryUrl);//Interrogate the API based on the query string.
         }
     }
 }
